87d326eabb94404978e37b12d52ffef5d1fb5431
sam <contact@justsam.io>
2023-08-07 09:42:52 +0200

    refactor: byte counter

--- src/generator.rs
+++ src/generator.rs
@@ -1,40 +1,106 @@
-use std::marker::PhantomData;
-use std::ops::RangeFrom;
+use std::ops::Range;
 
-pub trait Gen {
-    type Output;
-
-    fn next(&mut self) -> Option<Self::Output>;
-}
+use crate::counter::ByteCounter;
 
 #[derive(
     Clone,
     Debug, 
 )]
-pub struct Generator<T> {
-    generator: RangeFrom<u128>,
-    marker: PhantomData<T>,
+pub struct Generator<'a> {
+    generator: Range<ByteCounter<'a>>,
 }
 
-impl<T: From<u128>> Generator<T> {
+impl<'a> Generator<'a> {
     pub fn new() -> Self {
         Generator {
-            generator: 0..,
-            marker: PhantomData,
+            generator: Range { 
+                start: ByteCounter::new(), 
+                end: ByteCounter::new(),
+            },
+        }
+    }
+    
+    pub fn new_with_prefix(prefix: &'a str) -> Self {
+        Generator {
+            generator: Range { 
+                start: ByteCounter::new_with_prefix(prefix), 
+                end: ByteCounter::new_with_prefix(prefix) 
+            },
         }
     }
 }
 
-impl<T: From<u128>> Gen for Generator<T> {
-    type Output = T;
+impl<'a> Default for Generator<'a> {
+    fn default() -> Self {
+        Generator::new()
+    }
+}
 
-    fn next(&mut self) -> Option<Self::Output> {
+impl<'a> Iterator for Generator<'a> {
+    type Item = ByteCounter<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         self.generator.next().map(|i| i.into())
     }
 }
 
-impl<T: From<u128>> Default for Generator<T> {
-    fn default() -> Self {
-        Generator::new()
+impl<'a> DoubleEndedIterator for Generator<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.generator.next_back().map(|i| i.into())
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use rand::distributions::{Alphanumeric, DistString};
+    use crate::generator::Generator;
+    use crate::counter::ByteCounter;
+
+    #[test]
+    fn test_generator() {
+        let prefix: String = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+
+        {
+            let generator = Generator::new();
+            let mut expected = ByteCounter::default();
+            for item in generator.take(1000) {
+                assert_eq!(item, expected);
+                assert_eq!(item.prefix, None);
+                expected = expected.next_id();
+            }
+        }
+
+        {
+            let generator = Generator::new();
+            let mut expected = ByteCounter::max();
+            for item in generator.rev().take(100) {
+                assert_eq!(item, expected);
+                assert_eq!(item.prefix, None);
+                expected = expected.prev_id();
+            }
+        }
+
+        {
+            let generator = Generator::new_with_prefix(&prefix);
+            let mut expected = ByteCounter::new_with_prefix(&prefix);
+            for item in generator.take(100) {
+                assert_eq!(item, expected);
+                assert_eq!(item.prefix, Some(prefix.as_ref()));
+                expected = expected.next_id();
+            }
+        }
+
+        {
+            let generator = Generator::new_with_prefix(&prefix);
+            let mut expected = ByteCounter::max_with_prefix(&prefix);
+            for item in generator.rev().take(100) {
+                println!("{:?}", item.to_string());
+                assert_eq!(item, expected);
+                assert_eq!(item.prefix, Some(prefix.as_ref()));
+                expected = expected.prev_id();
+            }
+        }
+    }
+
 }
---
+++ src/timestamp.rs
@@ -0,0 +1,54 @@
+use serde_derive::{Serialize, Deserialize};
+use std::hash::Hash;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn epoch_ns() -> u128 {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(time) => time.as_nanos(),
+        Err(_) => panic!("Unable to determine time."),
+    }
+}
+
+pub fn epoch_secs() -> u64 {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(time) => time.as_secs(),
+        Err(_) => panic!("Unable to determine time."),
+    }
+}
+
+#[derive(
+    Clone,
+    Copy,
+    Debug, 
+    Deserialize, 
+    Eq,
+    Hash, 
+    Ord,
+    PartialEq, 
+    PartialOrd,
+    Serialize, 
+)]
+pub struct Timestamp(u64);
+
+impl Timestamp {
+    pub fn new() -> Self {
+        Self(epoch_secs())
+    }
+
+    pub fn value(&self) -> u64 {
+        self.0
+    }
+}
+
+impl Default for Timestamp {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl From<&str> for Timestamp {
+    fn from(s: &str) -> Self {
+        let value = s.parse::<u64>().unwrap_or_else(|_| 0);
+        Self(value)
+    }
+}
